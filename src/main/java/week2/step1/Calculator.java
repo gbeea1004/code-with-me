fa03cda70e000746596269031c371cfcf799322f
SUNGJUNGHOON <corqkrtk1@gmail.com>
2021-07-15 17:06:08 +0900

    계산기미션 구현

--- src/main/java/week2/step1/Calculator.java
+++ src/main/java/week2/step1/Calculator.java
@@ -4,21 +4,21 @@ public class Calculator {
 
     // TODO: 두 수 덧셈 계산 로직을 구현한다.
     public int plus(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     // TODO: 두 수 뺄셈 계산 로직을 구현한다.
     public int minus(int a, int b) {
-        return 0;
+        return a - b;
     }
 
     // TODO: 두 수 곱셈 계산 로직을 구현한다.
     public int times(int a, int b) {
-        return 0;
+        return a * b;
     }
 
     // TODO: 두 수 나눗셈 계산 로직을 구현한다.
     public int divide(int a, int b) {
-        return 0;
+        return a / b;
     }
 }
