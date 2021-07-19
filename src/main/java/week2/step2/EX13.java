da8493be6e227249ec9a51aacd9b1fca83e3cbc2
SUNGJUNGHOON <corqkrtk1@gmail.com>
2021-07-19 15:50:13 +0900

    EX13

---
+++ src/main/java/week2/step2/EX13.java
@@ -0,0 +1,9 @@
+package week2.step2;
+
+public class EX13 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+    }
+}
