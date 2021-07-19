72835156362d7e959ee25ba574a07d58418fd68d
SUNGJUNGHOON <corqkrtk1@gmail.com>
2021-07-19 16:01:01 +0900

    Ex13 수정본

--- src/main/java/week2/step2/Ex13.java
+++ src/main/java/week2/step2/Ex13.java
@@ -1,6 +1,6 @@
 package week2.step2;
 
-public class EX13 {
+public class Ex13 {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             System.out.println(i);
