ef930ce1be62816409c27afb9efbf2d0c1277755
gbeea1004 <jvckigh@gmail.com>
2021-07-20 10:52:34 +0900

    fix: TODO 내용 오류 수정

--- src/main/java/week3/step2/Main.java
+++ src/main/java/week3/step2/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // TODO: 1. 해당 패키지에 Car 클래스를 만든다.
         // TODO: 2. Car 클래스는 현재 위치를 나타내는 int location, 엔진 출력을 나타내는 int power 속성을 가지고 있다. (속성은 private 접근 제어자로 구현한다.)
         // TODO: 3. Car 클래스의 생성자를 만들어준다. (location, power 매개 변수를 받도록 구현한다.)
-        // TODO: 4. Car 클래스 안에 자동차를 이동시키는 메서드를 추가한다. ex) public int go() { }
+        // TODO: 4. Car 클래스 안에 자동차를 이동시키는 메서드를 추가한다. ex) public void go() { }
         // TODO: 5. go() 메서드에 자동차의 출력에 따라 이동거리가 달라지도록 구현한다. ex) power 가 0 ~ 30 이면 location 2증가, power 가 31 ~ 60 이면 location 5증가, power 가 61 이상 이면 location 10증가
         // TODO: 6. Car 클래스 안에 현재 location 을 가져오는 메서드를 추가한다. ex) public int getLocation() { }
         // TODO: 6. 이곳에서 (main 메서드) Car 객체 3개 (bmw320i, genesisG80, avante)를 만들고 각각의 car 객체마다 go 메서드를 호출해준다.
