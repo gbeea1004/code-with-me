37b30875ac7f5a4a9d9a4c1c57853b90a1e635ff
gbeea1004 <jvckigh@gmail.com>
2021-07-28 22:09:20 +0900

    docs: 4주차 - 수요일 과제 추가

---
+++ src/main/java/week4/step1/Step6.java
@@ -0,0 +1,13 @@
+package week4.step1;
+
+public class Step6 {
+    public static void main(String[] args) {
+        // TODO: 이중 for 문 익숙해지기 (아래 그림을 console 창에 출력하시오.)
+        // *****
+        //  ****
+        //   ***
+        //    **
+        //     *
+
+    }
+}
